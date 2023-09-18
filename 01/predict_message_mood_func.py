19ffbc63db47251de60c05c30b682519693fcd5d
Irina Guseva <iguseva@media1.ru>
2023-09-18 10:39:55 +0300

    Added PM function, unittests and coverage file

--- 01/predict_message_mood_func.py
+++ 01/predict_message_mood_func.py
@@ -1,9 +1,10 @@
 import random
 
+
 class SomeModel:
     def predict(self, message: str) -> float:
-        # реализация не важна
         return random.uniform(0, 1)
+
 
 def predict_message_mood(
     message: str,
@@ -11,10 +12,19 @@ def predict_message_mood(
     bad_thresholds: float = 0.3,
     good_thresholds: float = 0.8,
 ) -> str:
+
+    if not isinstance(model, SomeModel):
+        raise TypeError('Ошибка. Функции на вход подана не модель SomeModel')
     pred = model.predict(message)
+    if not isinstance(pred, (int, float)):
+        raise TypeError("Ошибка. Pred должен быть числом")
+    if not isinstance(bad_thresholds, (int, float)):
+        raise TypeError("Ошибка. bad_thresholds должен быть числом")
+    if not isinstance(good_thresholds, (int, float)):
+        raise TypeError("Ошибка. good_thresholds должен быть числом")
+    assert bad_thresholds < good_thresholds, 'Некорректные значения bad_thresholds, good_thresholds'
     if pred < bad_thresholds:
-        return 'неуд'
-    elif pred > good_thresholds:
-        return 'отл'
-    else:
-        return 'норм'
+        return "неуд"
+    if pred > good_thresholds:
+        return "отл"
+    return "норм"
---
+++ 01/predict_message_mood_unittests.py
@@ -0,0 +1,108 @@
+import unittest
+from unittest.mock import MagicMock
+from predict_message_mood_func import predict_message_mood, SomeModel
+
+class TestPredictMessageMood(unittest.TestCase):
+
+    def setUp(self):
+        self.model = SomeModel()
+
+
+    def test_predict_message_mood_bad(self):
+        self.model.predict = MagicMock(return_value=0.2)
+        result = predict_message_mood("Тестируем получение оценки неуд", self.model)
+        self.assertEqual(result, 'неуд')
+
+
+    def test_predict_message_mood_good(self):
+        self.model.predict = MagicMock(return_value=0.9)
+        result = predict_message_mood("Тестируем получение оценки отл", self.model)
+        self.assertEqual(result, 'отл')
+
+
+    def test_predict_message_mood_normal(self):
+        self.model.predict = MagicMock(return_value=0.5)
+        result = predict_message_mood("Тестируем получение оценки норм", self.model)
+        self.assertEqual(result, 'норм')
+
+
+    def test_predict_message_mood_raises_error_when_missing_arguments(self):
+        with self.assertRaises(TypeError):
+            predict_message_mood()
+
+        with self.assertRaises(TypeError):
+            predict_message_mood(model=self.model)
+
+        with self.assertRaises(TypeError):
+            predict_message_mood(message="Hello")
+
+
+    def test_predict_message_mood_with_another_thresholds(self):
+        self.model.predict = MagicMock(return_value=1.1)
+        result = predict_message_mood(message="This is a normal message", model=self.model,
+                                      bad_thresholds=5, good_thresholds=10)
+        self.assertEqual(result, 'неуд')
+
+        self.model.predict = MagicMock(return_value=5.5)
+        result = predict_message_mood(message="This is a normal message", model=self.model,
+                                      bad_thresholds=5, good_thresholds=10)
+        self.assertEqual(result, 'норм')
+
+        self.model.predict = MagicMock(return_value=11)
+        result = predict_message_mood(message="This is a normal message", model=self.model,
+                                      bad_thresholds=5, good_thresholds=10)
+        self.assertEqual(result, 'отл')
+
+
+    def test_predict_message_mood_giving_prediction_of_wrong_type(self):
+        self.model.predict = MagicMock(return_value='МодельПетрович')
+        with self.assertRaises(TypeError):
+            predict_message_mood(
+                "Тестируем передачу str вместо корректного результата работы модели", 
+                self.model)
+
+        self.model.predict = MagicMock(return_value={})
+        with self.assertRaises(TypeError):
+            predict_message_mood(
+                "Тестируем передачу dict() вместо корректного результата работы модели", 
+                self.model)
+
+        self.model.predict = MagicMock(return_value=[])
+        with self.assertRaises(TypeError):
+            predict_message_mood(
+                "Тестируем передачу list() вместо корректного результата работы модели", 
+                self.model)
+
+
+    def test_predict_message_mood_giving_smth_instead_of_model(self):
+        with self.assertRaises(TypeError):
+            predict_message_mood(message='bool_model',
+                                 model=True)
+
+
+    def test_predict_message_mood_giving_bad_threshold_of_wrong_type(self):
+        with self.assertRaises(TypeError):
+            predict_message_mood(message='incorrect_bad_thresholds',
+                                 model=self.model,
+                                 bad_thresholds='bad',
+                                 good_thresholds=0.8)
+
+
+    def test_predict_message_mood_giving_good_threshold_of_wrong_type(self):
+        with self.assertRaises(TypeError):
+            predict_message_mood(message='incorrect_good_thresholds',
+                                 model=self.model,
+                                 bad_thresholds=0.4,
+                                 good_thresholds='good')
+
+
+    def test_predict_message_mood_giving_incorrect_thresholds(self):
+        with self.assertRaises(AssertionError):
+            predict_message_mood(message='incorrect_thresholds',
+                             model=self.model,
+                             bad_thresholds=0.5,
+                             good_thresholds=0.1)
+
+
+if __name__ == '__main__':
+    unittest.main()
