a5b75349bb5d4e4c9ff7be601bb5f674e0987b4e
Irina Guseva <iguseva@media1.ru>
2023-10-11 20:26:50 +0300

    Added CustomMeta

---
+++ 04/custommeta.py
@@ -0,0 +1,30 @@
+class CustomMeta(type):
+    def __new__(mcs, name, bases, init_dic):
+
+        new_dict = {
+             (("custom_" + key), key)
+             [key.startswith('__') and
+              key.endswith('__')]: init_dic[key]
+             for key in
+             init_dic.keys()
+         }
+
+        new_dict["__setattr__"] = lambda ex, attr, val: ex.__dict__.update({("custom_" + attr): val})
+        return super().__new__(mcs, name, bases, new_dict)
+
+    def __call__(cls, *args, **kwargs):
+        instance = super().__call__(*args, **kwargs)
+        return instance
+
+
+class CustomClass(metaclass=CustomMeta):
+    x = 50
+
+    def __init__(self, val=99):
+        self.val = val
+
+    def line(self):
+        return 100
+
+    def __str__(self):
+        return "Custom_by_metaclass"
